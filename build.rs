c6884e5910a39758defe79b3592b8d89a1a6e969
Soupborsh <100529201+Soupborsh@users.noreply.github.com>
2026-08-31 23:04:17 +0500

    Cleanup, refactor build.rs.

--- build.rs
+++ build.rs
@@ -1,27 +1,25 @@
+use std::env;
 use std::fs::copy;
-use std::path::PathBuf;
-use std::{env, fs};
 
 fn main() {
     let is_app = env::var_os("CARGO_FEATURE_APP").is_some();
     let in_ram = env::var_os("CARGO_FEATURE_IN_RAM").is_some();
-    if is_app {
-        copy("memory_app.x", "memory.x").unwrap();
-    } else {
-        copy("memory_bare.x", "memory.x").unwrap();
-    }
+
     if in_ram {
         copy("memory_ram.x", "memory.x").unwrap();
-        let out = PathBuf::from(env::var("OUT_DIR").unwrap());
-        fs::write(out.join("link_ram.x"), include_bytes!("link_ram.x")).unwrap();
-        println!("cargo:rustc-link-search={}", out.display());
-
-        println!("cargo:rerun-if-changed=link_ram.x");
         println!("cargo:rustc-link-arg-bins=-Tlink_ram.x");
+        println!("cargo:rerun-if-changed=link_ram.x");
     } else {
+        if is_app {
+            copy("memory_app.x", "memory.x").unwrap();
+        } else {
+            copy("memory_bare.x", "memory.x").unwrap();
+        }
         println!("cargo:rustc-link-arg-bins=-Tlink.x");
     }
+
     println!("cargo::rerun-if-changed=build.rs");
     println!("cargo::rerun-if-changed=memory_app.x");
     println!("cargo::rerun-if-changed=memory_bare.x");
+    println!("cargo::rerun-if-changed=memory_ram.x");
 }
